add798f1cdf32496a9f309f8183e081d39bb1b8c
Tassio Med <tassiohfmed@gmail.com>
2022-10-14 14:50:36 -0300

    feat: Navbar estilizada

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,53 @@
 import React,{ useState } from "react";
 import { Link } from 'react-scroll'
+// import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
 
 
 import { HiMenu } from 'react-icons/hi';
-import { GrClose } from 'react-icons/gr';
+import { MdClose } from 'react-icons/md';
 
 function Nav(){
   const [navbar, setNavbar] = useState(false);
   const handleClick = () => setNavbar(!navbar);
 
-  // const handleClose = () => setNavbar(!navbar);
+  const handleClose = () => setNavbar(!navbar);
 
   return(
-    <nav className="z-20 fixed flex w-screen h-28 bg-blue-900 drop-shadow-xl">
+    <nav className="w-screen h-20 z-10 bg-[#001933] fixed shadow-xl">
 
       <div className="px-2 flex justify-between items-center w-full h-full">
-        <div className="flex items-center">
-          <h1 className="p-4 relative list-none font-bold text-5xl tm tm:hover"><Link href="#hero" className="tracking-wider">tm.</Link></h1>
-          <ul className="hidden md:flex">
-            <li className="p-2 flex justify-center rounded-3xl list-none transition- hover:bg-blue-700 duration-700">
+        <div className="flex items-center w-screen">
+          <h1 className="p-4 mr-4 font-bold text-2xl tm tm:hover md:text-3xl">
+            <Link to="home" smooth={true} duration={500} className="tracking-wider">tm.</Link>
+          </h1>
+          <ul className="hidden text-slate-100 w-full justify-end md:flex">
+            <li className="p-2 mr-6 rounded-lg list-none transition- hover:bg-blue-700 duration-700">
               <Link to="home" smooth={true} duration={500}>Meus projetos</Link>
             </li>
-            <li className="p-2 rounded-3xl list-none hover:bg-blue-700 duration-700">
+            <li className="p-2 mr-6 rounded-lg list-none hover:bg-blue-700 duration-700">
               <Link to="sobre" smooth={true} offset={-200} duration={500}>Quem sou eu?</Link>
             </li>
-            <li className="p-2 rounded-3xl list-none hover:bg-blue-700 duration-700">
+            <li className="p-2 mr-4 rounded-lg list-none hover:bg-blue-700 duration-700">
               <Link to="trabalhos" smooth={true} offset={-100} duration={500}>Fale comigo</Link>
             </li>
           </ul>
         </div>
 
-        <div className="md:hidden mr-4" onClick={handleClick}>
-          {!navbar ? <HiMenu className="w-5" /> : <GrClose className="w-5" />}
+        <div className="text-slate-50 md:hidden mr-4" onClick={handleClick}>
+          {!navbar ? <HiMenu className="text-xl cursor-pointer" /> : <MdClose className="text-2xl text-slate-50 cursor-pointer" />}
         </div>
       </div>
 
-      <ul className={!navbar ? "hidden" : "absolute bg-blue-900 w-full px-8"}>
-        <li className="p-2 flex justify-center rounded-3xl list-none transition- hover:bg-blue-700 duration-700">
-          <Link to="home" smooth={true} duration={500}>Meus projetos</Link>
+      <ul className={!navbar ? "hidden" : "absolute bg-[#001933] w-full px-8 text-slate-50"}>
+        <li className="p-2 mb-3 rounded-lg list-none transition- hover:bg-blue-700 duration-700">
+          <Link to="home" smooth={true} duration={500} onClick={handleClose}>Meus projetos</Link>
         </li>
-        <li className="p-2 rounded-3xl list-none hover:bg-blue-700 duration-700">
-          <Link to="sobre" smooth={true} offset={-200} duration={500}>Quem sou eu?</Link>
+        <li className="p-2 mb-3 rounded-lg list-none hover:bg-blue-700 duration-700">
+          <Link to="sobre" smooth={true} offset={-200} duration={500} onClick={handleClose}>Quem sou eu?</Link>
         </li>
-        <li className="p-2 rounded-3xl list-none hover:bg-blue-700 duration-700">
-          <Link to="trabalhos" smooth={true} offset={-100} duration={500}>Fale comigo</Link>
+        <li className="p-2 mb-3 rounded-lg list-none hover:bg-blue-700 duration-700">
+          <Link to="trabalhos" smooth={true} offset={-100} duration={500} onClick={handleClose}>Fale comigo</Link>
         </li>
       </ul>
     </nav>
