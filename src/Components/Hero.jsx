946f11a2b42f37a09119e692fafe123002825bf1
Tassio Med <tassiohfmed@gmail.com>
2022-10-14 22:48:51 -0300

    feat: Hero estilizado e responsivo

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,10 +6,10 @@ function Hero(){
     <div className="w-full h-screen bg-[#001021] flex flex-col justify-between" id="hero">
       <div className="grid md:grid-cols-2 max-w-[77.5rem] m-auto">
         <div className='flex flex-col justify-center items-center md:items-start w-full px-2 py-24'>
-          <h1 className='text-[#5fadfc] text-6xl mb-10 font-bold md:text-7xl'>Bem-vindo ao meu Portfólio</h1>
-          <p className='text-lg text-slate-50 md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero totam quas quasi cum, ut voluptates excepturi esse. Ut eum, minima similique cumque ea alias odio cupiditate dolorum illum! Iste.</p>
+          <h1 className='text-center text-[#5fadfc] text-6xl mb-10 font-bold md:text-start md:text-7xl'>Bem-vindo ao meu Portfólio</h1>
+          <p className='text-center text-lg text-slate-50 md:text-start md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero totam quas quasi cum, ut voluptates excepturi esse. Ut eum, minima similique cumque ea alias odio cupiditate dolorum illum! Iste.</p>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center relative z-30 bottom-44 md:z-10 md:bottom-0">
           <img src={Hand} alt="hand-shake" className="w-[600px] mt-10 md:w-[800px]"/>
         </div>
       </div>
