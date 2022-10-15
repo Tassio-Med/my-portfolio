6f6bcd6727fa7aab68782743e65bf92d14d129d0
Tassio Med <tassiohfmed@gmail.com>
2022-10-14 22:47:09 -0300

    feat: arquivo de dados dos projetos adicionado e importado

---
+++ src/data.js
@@ -0,0 +1,66 @@
+import Image1 from './Images/background.png'
+
+export const projectsData = [
+  {
+    id:1,
+    title: 'project 1',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id:2,
+    title: 'project 2',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id:3,
+    title: 'project 3',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 4,
+    title: 'project 4',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 5,
+    title: 'project 5',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 6,
+    title: 'project 6',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 7,
+    title: 'project 7',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 8,
+    title: 'project 8',
+    skills: 'Redux',
+    img: Image1,
+  },
+
+  {
+    id: 9,
+    title: 'project 9',
+    skills: 'Redux',
+    img: Image1,
+  },
+]
