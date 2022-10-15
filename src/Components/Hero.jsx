581dbecda4b24774f07b0bb470200cf36d3c1eab
Tassio Med <tassiohfmed@gmail.com>
2022-10-14 23:20:06 -0300

    fix: z-index do imgem ajustado

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero(){
           <h1 className='text-center text-[#5fadfc] text-6xl mb-10 font-bold md:text-start md:text-7xl'>Bem-vindo ao meu Portfólio</h1>
           <p className='text-center text-lg text-slate-50 md:text-start md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero totam quas quasi cum, ut voluptates excepturi esse. Ut eum, minima similique cumque ea alias odio cupiditate dolorum illum! Iste.</p>
         </div>
-        <div className="flex justify-center items-center relative z-30 bottom-44 md:z-10 md:bottom-0">
+        <div className="flex justify-center items-center relative z-10 bottom-44 md:z-10 md:bottom-0">
           <img src={Hand} alt="hand-shake" className="w-[600px] mt-10 md:w-[800px]"/>
         </div>
       </div>
