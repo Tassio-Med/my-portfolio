5a3a829af6762891d97512ba10c87632343a4e3f
Tassio Med <tassiohfmed@gmail.com>
2022-10-17 12:30:34 -0300

    feat: função de notificação do react-toastfy adicionada

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 import emailjs from '@emailjs/browser';
+
 import { SiLinkedin, SiTelegram, SiGithub, SiWhatsapp, SiGmail, SiGooglemaps } from "react-icons/si"
 
 
 function ContactMe() {
 
-  const [result, showResult] = useState(false);
-
   const sendEmail = (e) => {
     e.preventDefault();
+
+    toast.success("Mensagem enviada com sucesso!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
 
     emailjs.sendForm(
       'service_9g71ut5',
@@ -27,23 +33,12 @@ function ContactMe() {
           console.log(error.text);
       });
       e.target.reset();
-      showResult(true);
-
     };
-    // setTimeout(() => { showResult(false) },7000);
-    
-    const showToastMessage = () => {
-      toast.success('Mensagem enviada com sucesso !', {
-          position: toast.POSITION.TOP_RIGHT
-      });
-    };
-
-    const validMessage = result ? showToastMessage : null;
  
 
   return(
     <div id="contact" className="w-full h-full mt-10 block justify-center items-center md:flex md:justify-center md:items-center md:mt-28 bg-[#001021]">
-      <div className='text-slate-50 p-4 flex flex-col justify-center items-center md:flex md:justify-start md:items-start md:w-[450px] '>
+      <div className='mb-10 text-slate-50 p-4 flex flex-col justify-center items-center md:flex md:justify-start md:items-start md:w-[450px] '>
         <h2 className='text-4xl font-bold text-[#5fadfc] md:text-5xl'>Fale comigo</h2>
         <p className='text-lg text-center md:text-start'>
           Me envie uma mensagem pelas redes e conecte-se comigo.
@@ -56,7 +51,7 @@ function ContactMe() {
             rel="noopener noreferrer"
             className='pr-6'
           >
-            <SiLinkedin className='text-xl'/>
+            <SiLinkedin className='text-xl hover:scale-110 ease-in-out duration-300'/>
           </a>
 
           <a 
@@ -65,7 +60,7 @@ function ContactMe() {
             rel="noopener noreferrer"
             className='pr-6'
             >
-            <SiGithub className='text-xl' />
+            <SiGithub className='text-xl hover:scale-110 ease-in-out duration-300' />
           </a>
 
           <a
@@ -74,7 +69,7 @@ function ContactMe() {
             rel="noopener noreferrer"
             className='pr-6'
           >
-            <SiTelegram className='text-xl'/>
+            <SiTelegram className='text-xl hover:scale-110 ease-in-out duration-300'/>
           </a>
 
 
@@ -84,7 +79,7 @@ function ContactMe() {
             rel="noopener noreferrer"
             className='pr-6'
             >
-            <SiWhatsapp className='text-xl'/>
+            <SiWhatsapp className='text-xl hover:scale-110 ease-in-out duration-300'/>
           </a>
           <a
             href="mailto:tassiohfmed@gmail.com"
@@ -92,13 +87,13 @@ function ContactMe() {
             rel="noopener noreferrer"
             className='pr-6'
           >
-            <SiGmail className='text-xl'/>
+            <SiGmail className='text-xl hover:scale-110 ease-in-out duration-300'/>
           </a>
 
         </div>
         <div className='mt-9'>
           <a
-            className='flex items-center'
+            className='flex items-center hover:scale-110 ease-in-out duration-300'
             href="https://goo.gl/maps/GrXae3ium4mfmytE9"
             target="_blank"
             rel="noopener noreferrer"
@@ -112,29 +107,27 @@ function ContactMe() {
         <div className="w-full text-[#001021]">
           <label htmlFor="name">Nome</label>
           <br/>
-          <input type="text" name="fullName" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" required='true'placeholder="Nome completo"/>
+          <input type="text" name="fullName" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" required={true} placeholder="Nome completo"/>
         </div>
         <br/>
         <div className="w-full text-[#001021]">
           <label htmlFor="email" >Email</label>
           <br/>
-          <input type="email" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" required='true' placeholder="email.example@gmail.com" />
+          <input type="email" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" required={true} placeholder="email.example@gmail.com" />
         </div>
         <br/>
         
         <div className="w-full text-[#001021]">
           <label htmlFor="message">Mensagem</label>
           <br/>
-          <textarea className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" name="message" rows="5" type="message" required='true'/>
+          <textarea className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#5fadfc] focus:outline-none focus:ring-0" name="message" rows="5" type="message" required={true}/>
         </div>
         <br/>
         <div className='w-full'>
-          { result ?  validMessage : (
-            <button type="submit" className="bg-[#0c63ba] hover:bg-[#1c87f3]  rounded-lg w-full py-2 font-bold text-slate-50">
-              Enviar
-            </button>
-            ) }
-          {/* <ToastContainer /> */}
+          <button type="submit" className="bg-[#0c63ba] hover:bg-[#1c87f3]  rounded-lg w-full py-2 font-bold text-slate-50">
+            Enviar
+          </button>
+          <ToastContainer />
         </div>
       </form>
     </div>
