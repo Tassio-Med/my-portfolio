54b4031172da6b478ad172adcd7ee0ab70913bbd
Tassio Med <tassiohfmed@gmail.com>
2022-10-17 00:13:29 -0300

    build: rota do componente Footer adicionada

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import About from "./Components/About";
 import Nav from "./Components/Navbar"
 import Hero from "./Components/Hero";
 import Carousel from "./Components/Carousel";
-// import Contact from "./Components/Contact";
+import ContactMe from "./Components/ContactMe";
+import Footer from "./Components/Footer"
 // import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-      <div className="app">
+      <div className="bg-[#001021]">
         <Nav/>
         <Hero/>
         <Carousel/>
         <About/>
-        {/* <Contact/> */}
+        <ContactMe/>
+        <Footer/>
       </div>
   );
 }
