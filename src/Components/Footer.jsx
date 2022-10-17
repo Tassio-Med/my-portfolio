2af001136f4585e186609e03ff721076f9f16f3a
Tassio Med <tassiohfmed@gmail.com>
2022-10-17 12:29:26 -0300

    fix: erro de nomeação de classes corrigida

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer(){
   return(
     <div>
-      <footer class="p-4 rounded-lg shadow md:px-6 md:py-6">
-        <hr class="my-6 sm:mx-auto lg:my-8 border-t-slate-600"/>
-        <span class="block text-sm text-gray-500 sm:text-center ">
-          © 2022 <a href="https://www.linkedin.com/in/tassiomed98" target="_blank" rel="noreferrer"class="hover:underline">Tássio M.</a> All Rights Reserved.
+      <footer className="p-4 rounded-lg shadow md:px-6 md:py-6">
+        <hr className="my-6 sm:mx-auto lg:my-8 border-t-slate-600"/>
+        <span className="block text-sm text-center text-gray-500 sm:text-center ">
+          © 2022 <a href="https://www.linkedin.com/in/tassiomed98" target="_blank" rel="noreferrer"className="hover:underline">Tássio M.</a> All Rights Reserved.
         </span>
 	    </footer>
     </div>
