eeb98674b8e8c807ca67ab325a3dacce318e7fdf
Tassio-Med <tassiohfmed@gmail.como>
2023-01-18 17:57:41 -0300

    :sparkles: feat: mudança na apresentação

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hand from '../Images/3dhand.png'
-import Typical from 'react-typical'
 
 
 function Hero(){
@@ -8,23 +7,15 @@ function Hero(){
     <div className="w-full h-screen bg-[#001021] flex flex-col justify-between" id="hero">
       <div className="grid md:grid-cols-2 max-w-[77.5rem] m-auto">
         <div className='flex flex-col justify-center items-center md:items-start w-full px-2 py-24'>
-          <h1 className='text-center text-[#5fadfc] text-6xl mb-10 font-bold md:text-start md:text-7xl'>
-            Bem-vindo ao meu portfólio! {''}
+          <h1 className='text-center text-[#5fadfc] text-6xl mb-7 font-bold md:text-start md:text-7xl'>
+            Bem-vindo ao meu portfólio
           </h1>
-          <p className='text-center text-lg text-slate-50 md:text-start md:text-xl'>
-            Estou em um relacionamento sério com {''}
-            <Typical
-              loop={Infinity}
-              wrapper="b"
-              steps={[
-                'a tecnologia!', 3000,
-                'a programação!', 3000,
-                'o desenvolvimento web!', 3000,
-              ]}
-            />
-            <br />
-             Sou apaixonado por leitura e ilustração. Adoro música, basquete, artes marcias e assistir séries.
-             Fiz um cafézinho, vamos conversar?
+          <h2 className='text-center font-medium mb-3  text-slate-50 md:text-start md:text-xl'>
+            Aqui você pode conhecer  meus trabalhos. Fique à vontade!
+          </h2>
+          <p className='text-center text-lg font-thin text-slate-50 md:text-start md:text-xl'>
+            Sou apaixonado por leitura e ilustração. Adoro música, basquete, artes marcias e assistir séries.
+            Fiz um cafézinho, vamos conversar?
           </p>
         </div>
         <div className="flex justify-center items-center relative z-10 bottom-44 md:z-10 md:bottom-0">
