8c65752580a1333133f7259bafb212fc27808d08
Tassio-Med <tassiohfmed@gmail.como>
2023-01-18 14:55:26 -0300

    :sparkles: feat: skills substituídas pro descrição

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import Image1 from './Images/background.png'
+// import Image1 from './Images/background.png'
 import ToDo from './Images/todolist.png'
 import Movie from './Images/movie-library.png'
 import Finantial from './Images/finantial-system.png'
@@ -9,73 +9,78 @@ export const projectsData = [
   {
     id:1,
     title: 'To Do List',
-    skills: 'Redux',
+    skills: 'ReactJS, Javascript e CSS',
     img: ToDo,
     link: 'https://tazmed-todo-list.netlify.app/',
     github: 'https://github.com/Tassio-Med/react-todo-list',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, libero sed, quidem officia sequi sapiente fugit est, rem dolores',
   },
 
   {
     id:2,
     title: 'Movie Library',
-    skills: 'Redux',
+    skills: 'ReactJS, Javascript e TailwindCSS',
     img: Movie,
     link: 'https://tazmed-movielibray.netlify.app/',
     github: 'https://github.com/Tassio-Med/movies-library',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, libero sed, quidem officia sequi sapiente fugit est, rem dolores',
   },
 
   {
     id:3,
     title: 'Finantial System',
-    skills: 'Redux',
+    skills: 'Typescript, ReactJS e Styled-Components',
     img: Finantial,
     link: 'https://tazmed-finantial-system.netlify.app/',
     github: 'https://github.com/Tassio-Med/finance-system',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, libero sed, quidem officia sequi sapiente fugit est, rem dolores',
   },
 
   {
     id: 4,
-    title: 'Docker To Do List',
-    skills: 'Redux',
+    title: 'Docker-To Do List',
+    skills: 'Docker',
     img: Docker,
     link: 'https://github.com/Tassio-Med/docker-to-do-list-project',
     github: 'https://github.com/Tassio-Med/docker-to-do-list-project',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, libero sed, quidem officia sequi sapiente fugit est, rem dolores',
   },
 
   {
     id: 5,
     title: 'SQL-One For All',
-    skills: 'Redux',
+    skills: 'SQL, MySQL',
     img: OneForAll,
     link: 'https://github.com/Tassio-Med/sql-one-for-all-project',
     github: 'https://github.com/Tassio-Med/sql-one-for-all-project',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, libero sed, quidem officia sequi sapiente fugit est, rem dolores',
   },
 
-  {
-    id: 6,
-    title: 'project 6',
-    skills: 'Redux',
-    img: Image1,
-  },
+  // {
+  //   id: 6,
+  //   title: 'project 6',
+  //   skills: 'Redux',
+  //   img: Image1,
+  // },
 
-  {
-    id: 7,
-    title: 'project 7',
-    skills: 'Redux',
-    img: Image1,
-  },
+  // {
+  //   id: 7,
+  //   title: 'project 7',
+  //   skills: 'Redux',
+  //   img: Image1,
+  // },
 
-  {
-    id: 8,
-    title: 'project 8',
-    skills: 'Redux',
-    img: Image1,
-  },
+  // {
+  //   id: 8,
+  //   title: 'project 8',
+  //   skills: 'Redux',
+  //   img: Image1,
+  // },
 
-  {
-    id: 9,
-    title: 'project 9',
-    skills: 'Redux',
-    img: Image1,
-  },
+  // {
+  //   id: 9,
+  //   title: 'project 9',
+  //   skills: 'Redux',
+  //   img: Image1,
+  // },
 ]
