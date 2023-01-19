6fbef9da46ca1ab692f0ad213028d813929f393d
Tassio-Med <tassiohfmed@gmail.como>
2023-01-19 11:59:17 -0300

    :sparkles: feat: mudança no posicionamento do texto.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,14 +6,14 @@ function Hero(){
   return(
     <div className="w-full h-screen bg-[#001021] flex flex-col justify-between" id="hero">
       <div className="grid md:grid-cols-2 max-w-[77.5rem] m-auto">
-        <div className='flex flex-col justify-center items-center md:items-start w-full px-2 py-24'>
-          <h1 className='text-center text-[#5fadfc] text-6xl mb-7 font-bold md:text-start md:text-7xl'>
+        <div className='flex flex-col justify-center items-center md:ml-4 md:items-start w-full px-2 py-24'>
+          <h1 className='text-center text-[#5fadfc] text-7xl mb-7 font-bold md:text-start md:text-7xl'>
             Bem-vindo ao meu portfólio
           </h1>
-          <h2 className='text-center font-medium mb-3  text-slate-50 md:text-start md:text-xl'>
-            Aqui você pode conhecer  meus trabalhos. Fique à vontade!
+          <h2 className='text-center  mb-3  text-slate-50 md:text-start md:text-xl'>
+            Aqui você pode conhecer um pouco sobre mim e meus projetos. Fique à vontade!
           </h2>
-          <p className='text-center text-lg font-thin text-slate-50 md:text-start md:text-xl'>
+          <p className='text-center text-lg font-thin text-slate-50 md:text-start md:text-lg'>
             Sou apaixonado por leitura e ilustração. Adoro música, basquete, artes marcias e assistir séries.
             Fiz um cafézinho, vamos conversar?
           </p>
